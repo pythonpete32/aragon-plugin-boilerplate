0f0ed1637e8c75f4f831ad0e7fbe221af198afa5
pythonpete32 <aaron@blackscale.io>
2023-02-15 17:19:06 +0000

    remove junk scripts

--- scripts/tst.ts
+++
@@ -1,5 +0,0 @@
-import hre from "hardhat";
-import { activeContractsList } from "@aragon/core-contracts-ethers";
-
-console.log(hre.network.name);
-console.log(activeContractsList[hre.network.name]);
