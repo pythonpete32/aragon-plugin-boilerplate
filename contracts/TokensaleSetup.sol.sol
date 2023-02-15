c02eac5138fe5361a39abf2cf74ed449cfa9b00b
pythonpete32 <aaron@blackscale.io>
2023-02-15 10:49:24 +0000

    make tokensale setup

---
+++ contracts/TokensaleSetup.sol.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import {IDAO} from "aragon-plugin-base/contracts/lib/interfaces/IDAO.sol";
+import {DAO} from "aragon-plugin-base/contracts/lib/dao-authorizable/DAO.sol";
+import {PermissionLib} from "aragon-plugin-base/contracts/lib/permission/PermissionLib.sol";
+import {PluginSetup, IPluginSetup} from "aragon-plugin-base/contracts/lib/plugin/PluginSetup.sol";
+
+import {BoilerplatePlugin} from "./BoilerplatePlugin.sol";
+
+contract TokenSaleSetup is PluginSetup {
+    address private pluginBase;
+
+    constructor() {
+        pluginBase = address(new BoilerplatePlugin());
+    }
+
+    function prepareInstallation(
+        address _dao,
+        bytes memory _data
+    ) external returns (address plugin, PreparedDependency memory preparedDependency) {}
+
+    function prepareUninstallation(
+        address _dao,
+        SetupPayload calldata _payload
+    ) external view returns (PermissionLib.MultiTargetPermission[] memory permissions) {}
+
+    function getImplementationAddress() external view virtual override returns (address) {}
+}
